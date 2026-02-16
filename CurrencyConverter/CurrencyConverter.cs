2caf8123b482b136c11939ef6ef3d63f8e26456c
DaedanAlcantara <daedanryu@gmail.com>
2026-02-16 15:49:46 +0800

    New insertions

---
+++ CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,7 @@
+﻿using System;
+Console.Write("Enter amount in USD: ");
+double amountInUSD = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter exchange rate from USD to EUR: ");
+double exchangeRate = Convert.ToDouble(Console.ReadLine());
+double amountInEUR = amountInUSD * exchangeRate;
+Console.WriteLine("Amount in EUR: {0:F2}", amountInEUR);
---
+++ InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.Write("Enter the principal amount: ");
+double principal = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter rate of interest: ");
+double rate = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter time in years: ");
+double time = Convert.ToDouble(Console.ReadLine());
+
+double simpleInterest = (principal * rate * time) / 100;
+Console.WriteLine("Simple Interest: {0:F2}", simpleInterest);
