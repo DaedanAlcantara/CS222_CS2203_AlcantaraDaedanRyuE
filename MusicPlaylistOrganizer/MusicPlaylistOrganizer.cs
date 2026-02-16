c18111c43dc6738fb1f7c6ef64dce55c5ad22d13
DaedanAlcantara <daedanryu@gmail.com>
2026-02-16 15:31:32 +0800

    Add project files.

---
+++ MusicPlaylistOrganizer/MusicPlaylistOrganizer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.ComponentModel.Design;
+using static MusicPlaylistOrganizer;
+
+class MusicPlaylistOrganizer
+{
+    static void Main()
+    {
+        Console.Write("Songs to add: ");
+        int numberOfSongs = Convert.ToInt32(Console.ReadLine());
+        Song[] songs = new Song[numberOfSongs];
+        for (int i = 0; i < numberOfSongs; i++)
+        {
+
+            Console.Write("Title: ");
+            string Title = Console.ReadLine();
+            Console.Write("Artist: ");
+            string Artist = Console.ReadLine();
+            Console.Write("Duration: ");
+            string Duration = Console.ReadLine();
+            if (string.IsNullOrEmpty(Artist) && string.IsNullOrEmpty(Duration))
+            {
+                Song inputsong = new Song(Title);
+                songs[i] = inputsong;
+            }
+            else if (string.IsNullOrEmpty(Duration))
+            {
+                Song inputsong = new Song(Title, Artist);
+                songs[i] = inputsong;
+            }
+            else if (string.IsNullOrEmpty(Artist) && string.IsNullOrEmpty(Duration) && string.IsNullOrEmpty(Title))
+            {
+                Song inputsong = new Song();
+                songs[i] = inputsong;
+            }
+            else
+            {
+                Song inputsong = new Song(Title, Artist, Duration);
+                songs[i] = inputsong;
+            }
+
+
+            
+        }
+        Console.WriteLine("=== || MY PLAYLIST || ===");
+        Console.WriteLine("{0,-20}{1,-15}{2,-6}", "Title", "Artist", "Duration");
+        Console.WriteLine(new string('-', 60));
+        float totalDuration = 0;
+        foreach (var song in songs)
+        {
+
+            song.DisplaySong();
+            if (song.Duration != "Unknown")
+            {
+                totalDuration += float.Parse(song.Duration);
+            }
+
+
+        }
+
+        Console.WriteLine("Total Duration: {0:F2} mins", totalDuration);
+        float averageDuration = totalDuration / numberOfSongs;
+        Console.WriteLine("Average Duration: {0:F2} mins", averageDuration);
+    }
+
+    public class Song
+    {
+        public string Title;
+        public string Artist;
+        public string Duration;
+        //default
+        public Song()
+        {
+            Title = "Unknown";
+            Artist = "Unknown";
+            Duration = "Unknown";
+        }
+
+        //parameterized (three-parameter)
+        public Song(string title, string artist, string duration)
+        {
+            this.Title = title;
+            this.Artist = artist;
+            this.Duration = duration;
+        }
+
+        //two-parameter constructor
+        public Song(string title, string artist)
+        {
+            this.Title = title;
+            this.Artist = artist;
+            this.Duration = "0.00";
+        }
+
+        // one-parameter constructor delegating to the three-parameter constructor
+        public Song(string title) : this(title, "Unknown", "0.00")
+        {
+        }
+
+        
+        public void DisplaySong()
+        {
+            Console.WriteLine($"{Title,-20}{Artist,-15}{Duration,-6}");
+        }
+    }
+}
+
---
+++ PentagonClass/PentagonClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class PentagonClass
+{
+    static void Main(string[] args)
+    {
+        Pentagon p1 = new Pentagon();
+        Pentagon p2 = new Pentagon(5);
+        
+        Console.WriteLine("Pentagon p1");
+        Console.WriteLine(new string('-',25));
+        p1.Display();
+
+        Console.WriteLine("\nPentagon p2");
+        Console.WriteLine(new string('-',25));
+        p2.Display();
+
+
+    }
+}
+class Pentagon
+{
+    public double Side;
+    //default
+    public Pentagon()
+    {
+        Side = 0;
+    }
+    //Parameterized
+    public Pentagon(double Side)
+    {
+        this.Side = Side;
+    }
+    public double GetPerimeter(double Side)
+    {
+        return 5 * Side;
+    }
+    public double GetArea(double Side)
+    {
+        return (Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5))) * Math.Pow(Side, 2)) / 4;
+    }
+    public void Display()
+    {
+        Console.WriteLine("Side length: " + Side);
+        Console.WriteLine("Perimeter: {0:F3}", GetPerimeter(Side));
+        Console.WriteLine("Area: {0:F3}", GetArea(Side));
+    }
+}
